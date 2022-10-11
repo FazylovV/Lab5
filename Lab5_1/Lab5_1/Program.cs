1b659d0c91ff62fcd3fffbe74209f6e471456de3
vladi <vladi@LAPTOP-QRGAGD2K>
2022-10-11 12:50:36 +0500

    Add project files.

---
+++ Lab5_1/Lab5_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab51
+{
+    class Program
+    {
+        static public void Main()
+        {
+            double LastNum = 0;
+            while (true)
+            {
+                string str = Console.ReadLine();
+
+                bool IsInt = int.TryParse(str, out int currentNumInt);
+                bool IsDouble = double.TryParse(str, out double currentNumDouble);
+
+                if (IsInt)
+                {
+                    Console.WriteLine(currentNumInt);
+                    LastNum = currentNumInt;
+                }
+                else if (IsDouble)
+                {
+                    if (currentNumDouble == LastNum)
+                        break;
+                    else
+                    {
+                        LastNum = currentNumDouble;
+                        continue;
+                    }
+                }
+                else if (str == "q")
+                    break;
+                else
+                    continue;
+            }
+        }
+    }
+}
